c68dd1a46ab1c0b71c8793d2ef7eef745871917c
Phil <philipparold.pa@gmail.com>
2020-04-26 04:20:31 +0200

    added moq for mocking in unit tests

---
+++ UnitTestProject/MockTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Moq;
+using Xunit;
+
+namespace UnitTestProject
+{
+	public interface IFoo
+	{
+		Bar Bar { get; set; }
+		string Name { get; set; }
+		int Value { get; set; }
+		bool DoSomething(string value);
+		bool DoSomething(int number, string value);
+		string DoSomethingStringy(string value);
+		bool TryParse(string value, out string outputValue);
+		bool Submit(ref Bar bar);
+		int GetCount();
+		bool Add(int value);
+	}
+
+	public class Bar
+	{
+		public virtual Baz Baz { get; set; }
+
+		public virtual bool Submit()
+		{
+			return false;
+		}
+	}
+
+	public class Baz
+	{
+		public virtual string Name { get; set; }
+	}
+
+	public class MockTest
+	{
+		[Fact]
+		public void TestMock()
+		{
+			var mock = new Mock<IFoo>();
+			mock.Setup(foo => foo.DoSomething("ping")).Returns(true);
+
+			Assert.True(mock.Object.DoSomething("ping"));
+
+
+		}
+	}
+}
